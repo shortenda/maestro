c12ad252652b1ce8c970bc63be587f3fe5bca3a4
shortenda <shortenda>
2023-11-13 03:34:11 -0800

    Make playback slightly less wrong, but still bad

--- main.gd
+++ main.gd
@@ -5,6 +5,20 @@ const Utility = preload( "res://addons/midi/Utility.gd" )
 
 onready var midi_player:MidiPlayer = $MidiPlayer
 
+# var seconds_to_timebase = tempo / 60.0
+# var timebase_to_seconds = 60.0 / tempo
+
+func track_coroutine(track: SMF.MIDITrack):
+    print("Started coroutine for track", track.track_number)
+    var last_event_chunk_time = null
+    for event_chunk in track.events:
+        if last_event_chunk_time != null:
+            yield(get_tree().create_timer((event_chunk.time as float - last_event_chunk_time as float) / 200), "timeout")
+        $MidiPlayer.receive_raw_smf_midi_event(event_chunk.channel_number, event_chunk.event)
+        last_event_chunk_time = event_chunk.time
+
+var track_coroutines = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
     print ("test")
@@ -14,9 +28,4 @@ func _ready():
         print("Error loading midi file!")
     
     for track in result.data.tracks:
-        for event_chunk in track.events:
-            print(event_chunk.channel_number)
-            print(event_chunk.event.type)
-            $MidiPlayer.receive_raw_smf_midi_event(event_chunk.channel_number, event_chunk.event)
-            yield(get_tree().create_timer(0.001), "timeout")
-
+        track_coroutines.append(track_coroutine(track))
