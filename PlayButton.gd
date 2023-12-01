ef89d46df4f3c7dbe835596ad0eec3101ffec4e2
shortenda <shortenda@gmail.com>
2023-12-01 02:38:53 -0800

    More merge fixes

--- PlayButton.gd
+++ PlayButton.gd
@@ -3,11 +3,12 @@ extends TextureButton
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-    
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+    if (pressed == true):
+        get_tree().change_scene("res://3d_in_2d.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- QuitButton.gd
+++ QuitButton.gd
@@ -4,12 +4,12 @@ extends TextureButton
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-    
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-        
-    pass # Replace with function body.
+    if (pressed == true):
+        get_tree().quit()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
