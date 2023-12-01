7994575d4ff397f842a94ada725a5f3eadfb9809
Yasmine Lee <yasmine198@csu.fullerton.edu>
2023-11-30 23:50:32 -0800

    Updated UI + Added Main Menu
    - new main menu tscn
    - functional, changes to scenes (need to change default scene in project settings)
    - included new restart + return buttons for ending / game over screens

--- dialog_scene.gd
+++ dialog_scene.gd
@@ -15,17 +15,17 @@ export (Resource) var next_scene
 var current_line = 0
 
 func _unhandled_input(event):
-    if event is InputEventKey and event.pressed:
-        if event.scancode == KEY_ENTER:
-            # Load next line of dialog
-            $DialogNode.text = dialog.lines[0]
+	if event is InputEventKey and event.pressed:
+		if event.scancode == KEY_ENTER:
+			# Load next line of dialog
+			$DialogNode.text = dialog.lines[0]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    #for line in dialog.lines:
-    #    print(line)
-    # TODO remove this scene, and add the new scene
-    self.queue_free()
+	#for line in dialog.lines:
+	#    print(line)
+	# TODO remove this scene, and add the new scene
+	self.queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
