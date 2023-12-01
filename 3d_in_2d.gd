6b6b34e4ae52f4a99ba392122e997e9f9b585966
Yasmine Lee <yasmine198@csu.fullerton.edu>
2023-12-01 01:14:51 -0800

    Finished main menu w/ sounds and music
    Added button sounds
    Added music
    Changed tabs to spaces in settings
    Changed default scene to MainMenu.tscn

--- 3d_in_2d.gd
+++ 3d_in_2d.gd
@@ -4,15 +4,15 @@ extends Node2D
 var viewport_initial_size = Vector2()
 
 func _ready():
-	#warning-ignore:return_value_discarded
-	get_viewport().connect("size_changed", self, "_root_viewport_size_changed")
-	viewport_initial_size = $Viewport.size
+    #warning-ignore:return_value_discarded
+    get_viewport().connect("size_changed", self, "_root_viewport_size_changed")
+    viewport_initial_size = $Viewport.size
 
 # Called when the root's viewport size changes (i.e. when the window is resized).
 # This is done to handle multiple resolutions without losing quality.
 func _root_viewport_size_changed():
-	# The viewport is resized depending on the window height.
-	# To compensate for the larger resolution, the viewport sprite is scaled down.
-	$Viewport.size = Vector2.ONE * get_viewport().size.y
-	$NotesScene/ViewportSprite.scale = Vector2(1, 1) * viewport_initial_size.y / get_viewport().size.y
+    # The viewport is resized depending on the window height.
+    # To compensate for the larger resolution, the viewport sprite is scaled down.
+    $Viewport.size = Vector2.ONE * get_viewport().size.y
+    $NotesScene/ViewportSprite.scale = Vector2(1, 1) * viewport_initial_size.y / get_viewport().size.y
 
--- piano_notes.gd
+++ piano_notes.gd
@@ -21,52 +21,52 @@ export (NodePath) var score_tracker_node_path
 signal score_updated(misses)
 
 func _on_note_scheduled(note: Note):
-	pass
+    pass
 
 var _tracks: Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var standard_midi_file = SMF.new()
-	
-	var stream:StreamPeerBuffer = StreamPeerBuffer.new( )
-	stream.set_data_array((midi_bytes_resource as FileBytes).value)
-	stream.big_endian = true
-	var result = standard_midi_file.read_from_stream(stream)
+    var standard_midi_file = SMF.new()
+    
+    var stream:StreamPeerBuffer = StreamPeerBuffer.new( )
+    stream.set_data_array((midi_bytes_resource as FileBytes).value)
+    stream.big_endian = true
+    var result = standard_midi_file.read_from_stream(stream)
 
-	midi_bytes_resource = null
+    midi_bytes_resource = null
 
-	if result.error != OK:
-		print("Error loading midi file!")
-	
-	get_node("%SongProgress").smf_data = result.data
-	
-	var key_array = ["S", "D", "F", "J", "K", "L", "H"]
+    if result.error != OK:
+        print("Error loading midi file!")
+    
+    get_node("%SongProgress").smf_data = result.data
+    
+    var key_array = ["S", "D", "F", "J", "K", "L", "H"]
 
-	var track_i = 0
-	for key in key_array:               
-		var track = MidiSchdulerTrack.new(
-			key,
-			track_i,
-			get_node(self.track_container_node_path),
-			get_node(self.key_container_node_path),
-			self.owner
-		)
-		track.connect("tree_entered", track, "set_owner", [self.owner]);
-		self.add_child(track)
-		self._tracks.append(track)
-		track_i += 1
+    var track_i = 0
+    for key in key_array:               
+        var track = MidiSchdulerTrack.new(
+            key,
+            track_i,
+            get_node(self.track_container_node_path),
+            get_node(self.key_container_node_path),
+            self.owner
+        )
+        track.connect("tree_entered", track, "set_owner", [self.owner]);
+        self.add_child(track)
+        self._tracks.append(track)
+        track_i += 1
 
-	var midi_scheduler = MidiScheduler.new()
-	midi_scheduler.note_tracks = _tracks
-	midi_scheduler.connect("note_scheduled", get_node(score_tracker_node_path), "_on_note_scheduled")
-	midi_scheduler.midi_tracks = result.data.tracks
-	midi_scheduler.stage_length = get_node(self.track_container_node_path).rect_size.y
-	midi_scheduler.connect( "tree_entered", midi_scheduler, "set_owner", [self.owner]);
-	self.add_child(midi_scheduler)
-	
-	#get_node(self.track_container_node_path).get_node("TrackWindow").anchor_top = \
-	#    1.0 - get_node("%SongProgress").key_press_interval/get_node("%SongProgress").note_preview_time
-	
-	
+    var midi_scheduler = MidiScheduler.new()
+    midi_scheduler.note_tracks = _tracks
+    midi_scheduler.connect("note_scheduled", get_node(score_tracker_node_path), "_on_note_scheduled")
+    midi_scheduler.midi_tracks = result.data.tracks
+    midi_scheduler.stage_length = get_node(self.track_container_node_path).rect_size.y
+    midi_scheduler.connect( "tree_entered", midi_scheduler, "set_owner", [self.owner]);
+    self.add_child(midi_scheduler)
+    
+    #get_node(self.track_container_node_path).get_node("TrackWindow").anchor_top = \
+    #    1.0 - get_node("%SongProgress").key_press_interval/get_node("%SongProgress").note_preview_time
+    
+    
 
